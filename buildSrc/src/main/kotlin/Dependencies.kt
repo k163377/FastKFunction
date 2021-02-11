c915b8bbbf44bef6de5c007cedb0ff6385490062
wrongwrong <boranti1995@gmail.com>
2021-02-11 15:44:48 +0900

    依存関係の整理

--- buildSrc/src/main/kotlin/Dependencies.kt
+++
@@ -1,5 +0,0 @@
-object Dependencies {
-    object Kotlin {
-        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
-    }
-}
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,4 +2,8 @@ object Versions {
     const val projectVersion = "0.2.0-alpha"
 
     const val kotlinVersion = "1.4.30"
+    const val kotlinpoetVersion = "1.7.2"
+
+    const val autoCommonVersion = "0.11"
+    const val autoServiceVersion = "1.0-rc7"
 }
