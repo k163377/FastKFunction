2be8ae725445743f7ae38dfe175071160838471f
wrongwrong <boranti1995@gmail.com>
2021-02-11 15:11:01 +0900

    依存管理用モジュール等を追加

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,5 @@
+object Dependencies {
+    object Kotlin {
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
+    }
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,5 @@
+object Versions {
+    const val projectVersion = "0.2.0-alpha"
+
+    const val kotlinVersion = "1.4.30"
+}
